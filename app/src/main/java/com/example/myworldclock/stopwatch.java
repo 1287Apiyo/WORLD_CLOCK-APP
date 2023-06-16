fe4b7415f875991e72d539d9572ab5c1606b529d
Anne Apiyo Opiyo <93118744+1287Apiyo@users.noreply.github.com>
2023-06-16 14:08:20 +0300

    MY_WORLD_APP

--- app/src/main/java/com/example/myworldclock/stopwatch.java
+++ app/src/main/java/com/example/myworldclock/stopwatch.java
@@ -2,11 +2,87 @@ package com.example.myworldclock;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class stopwatch extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class stopwatch extends AppCompatActivity {
+
+    private TextView timerTextView;
+    private Button startButton;
+    private Button pauseButton;
+    private Button resetButton;
+
+    private long startTime = 0L;
+    private Handler handler = new Handler();
+    private long timeInMilliseconds = 0L;
+    private long timeSwapBuff = 0L;
+    private long updatedTime = 0L;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stopwatch);
+
+        timerTextView = findViewById(R.id.timerTextView);
+        startButton = findViewById(R.id.startButton);
+        pauseButton = findViewById(R.id.pauseButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTime = System.currentTimeMillis();
+                handler.postDelayed(updateTimerRunnable, 0);
+                startButton.setEnabled(false);
+                pauseButton.setEnabled(true);
+                resetButton.setEnabled(true);
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeSwapBuff += timeInMilliseconds;
+                handler.removeCallbacks(updateTimerRunnable);
+                startButton.setEnabled(true);
+                pauseButton.setEnabled(false);
+                resetButton.setEnabled(true);
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeSwapBuff = 0L;
+                startTime = 0L;
+                timerTextView.setText("00:00:00");
+                startButton.setEnabled(true);
+                pauseButton.setEnabled(false);
+                resetButton.setEnabled(false);
+            }
+        });
     }
+
+    private Runnable updateTimerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilliseconds = System.currentTimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int seconds = (int) (updatedTime / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            int milliseconds = (int) (updatedTime % 1000);
+
+            timerTextView.setText(String.format("%02d:%02d:%02d", minutes, seconds, milliseconds / 10));
+
+            handler.postDelayed(this, 0);
+        }
+    };
 }
+
